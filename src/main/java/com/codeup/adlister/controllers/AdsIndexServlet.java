b1c8af736a177e0769021674b3112fa032da1744
Cooper Hanke <cooper.hanke@gmail.com>
2018-04-09 11:56:07 -0500

    added universal search functionality - titles only displayed on main ads index page

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -12,7 +12,12 @@ import java.io.IOException;
 @WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        if (request.getParameter("search") != null) {
+            // TODO: Waiting on creating the actual method, standby
+            request.setAttribute("ads", DaoFactory.getAdsDao().findByText(request.getParameter("search")));
+        } else {
+            request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        }
         request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
     }
 }
